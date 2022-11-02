0e462c8c0160d01efb4ca8992f1c53adad07652f
nfdvv <nefedoff.vadim@gmail.com>
2022-11-02 17:37:56 +0300

    added section map

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,13 @@ $(function(){
   $('.product__wrapper-favorite').on('click', function(){
     $(this).toggleClass('product__wrapper-favorite--active')
   });
+  $('.tab').on('click', function(e){
+    e.preventDefault();
+    
+    $($(this).siblings()).removeClass('map__box-item--active');
+    $($(this).parent().siblings().find('div')).removeClass('max__box-map__item--active');
+
+    $(this).addClass('map__box-item--active');
+    $($(this).attr('href')).addClass('max__box-map__item--active');
+  });
 });
